b02c2e8663cdf9cab5eb48505cd67ba3ac64e4aa
Samueldamil <damilareedun@gmail.com>
2023-05-04 20:34:35 +0000

    file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,11 +61,12 @@ def before_request() -> str:
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if (not auth.authorization_header(request) and
-            not auth.session.cookie(request)):
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
-    request.current_user = auth.current_user(request)
-    if not request.current_user:
+
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
 
     request.current_user = current_user
