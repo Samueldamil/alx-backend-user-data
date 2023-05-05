48cc761465967c24b352788fb53104d30d3f3eee
Samueldamil <damilareedun@gmail.com>
2023-05-05 03:59:00 +0000

    file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Session Authentication
+""" Module of Session Authentication
 """
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -8,11 +7,11 @@ import uuid
 
 
 class SessionAuth(Auth):
-    """ Session Authentication class """
+    """Session Authentication Class"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Creates a Session ID for a user_id """
+        """Creates a Session ID for a user_id"""
 
         if user_id is None or not isinstance(user_id, str):
             return None
@@ -24,7 +23,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns a User ID based on a Session ID """
+        """Returns a User ID based on a Session ID"""
 
         if session_id is None or not isinstance(session_id, str):
             return None
@@ -32,7 +31,7 @@ class SessionAuth(Auth):
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """ returns a User instance based on a cookie value """
+        """Returns a User instance based on a cookie value"""
 
         session_id = self.session_cookie(request)
 
@@ -44,7 +43,8 @@ class SessionAuth(Auth):
         return User.get(user_id)
 
     def destroy_session(self, request=None):
-        """ deletes the user session / logout """
+        """Deletes de user session / logout"""
+
         if request is None:
             return False
 
